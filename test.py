0945fadd5f6820cb095b1210b4a9bb4536aef512
PepsiMonster <mikecelestialfiend@gmail.com>
2024-11-01 13:55:41 +0300

    send

--- test.py
+++
@@ -1,6 +0,0 @@
-def transfrom(meter,santimeter):
-    millimeter = meter*1000 + santimeter*10
-    return millimeter
-meter = int(input())
-santimeter = int(input())
-value = transfrom(meter,santimeter)
