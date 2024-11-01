e89ba175b177ad8106de4cab5c000e33a846e283
PepsiMonster <mikecelestialfiend@gmail.com>
2024-11-01 13:52:30 +0300

    julia added, python with classes done

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('hi')
+def transfrom(meter,santimeter):
+    millimeter = meter*1000 + santimeter*10
+    return millimeter
+meter = int(input())
+santimeter = int(input())
+value = transfrom(meter,santimeter)
